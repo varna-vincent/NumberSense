155e293ef4c849364ef4687030e34656752ce511
Varna <varnamvmv@gmail.com>
2017-03-18 19:21:51 -0700

    Added postscripts

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -28,7 +28,7 @@ var webpackConfig = merge(baseWebpackConfig, {
   plugins: [
     // http://vuejs.github.io/vue-loader/en/workflow/production.html
     new webpack.DefinePlugin({
-      'process.env': { NODE_ENV: '"production"' }
+      'process.env': env
     }),
     new webpack.optimize.UglifyJsPlugin({
       compress: {
